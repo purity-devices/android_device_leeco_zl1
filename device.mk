8a5311accf81a4eda76ed54cbb84e3cf908b0379
Marko Man <darkobas@gmail.com>
2017-05-19 14:37:12 +0200

    zl1: build libhealthd
    Change-Id: I04b41ff7c6f8db8546ac2a42b54385cd8725609f

--- device.mk
+++ device.mk
@@ -292,6 +292,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcommon_time_client
 
+#healthd
+PRODUCT_PACKAGES += \
+    libhealthd.msm8996
+
 #qdcm
 #PRODUCT_PACKAGES += \
     qdcm_calib_data_mdss_dsi_ft8716_1080p_video.xml \
